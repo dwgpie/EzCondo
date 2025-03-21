677b74c44ec59f96d3e048cd86b25d28237fbfc1
dwgpie <dungphan.170603@gmail.com>
2025-03-21 10:54:49 +0700

    fix UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,21 @@
 import SearchIcon from '@mui/icons-material/Search'
 import NotificationsIcon from '@mui/icons-material/Notifications'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { SearchContext } from '../Search/SearchContext'
 
 export default function Header() {
   const handleLogout = () => {
     localStorage.removeItem('token')
+  }
+
+  const searchContext = useContext(SearchContext)
+  if (!searchContext) return null
+
+  const { searchQuery, setSearchQuery } = searchContext
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value)
   }
 
   return (
@@ -16,10 +27,13 @@ export default function Header() {
           </span>
           <input
             type='text'
+            value={searchQuery}
+            onChange={handleChange}
             placeholder='Search'
             className='border border-gray-400 rounded-full pl-10 pr-4 py-2 w-[300px] bg-[#edf2f9] shadow-sm'
           />
         </div>
+
         <div className='flex gap-[30px] items-center'>
           <Link to='/login'>
             <button onClick={handleLogout}>Logout</button>
