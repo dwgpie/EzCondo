81ed5748c2aa402bda35d616f17d937fb2d62bbe
PhanMinhTuan282 <minhtuan.phan282@gmail.com>
2025-04-28 16:35:15 +0700

    update incident

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import SearchIcon from '@mui/icons-material/Search'
+import NotificationsIcon from '@mui/icons-material/Notifications'
 import { Link } from 'react-router-dom'
 import { useContext, useEffect, useState, useRef } from 'react'
 import { SearchContext } from '../Search/SearchContext'
@@ -13,7 +14,6 @@ import LogoutIcon from '@mui/icons-material/Logout'
 import PasswordIcon from '@mui/icons-material/Password'
 import * as signalR from '@microsoft/signalr'
 import { format, formatDistanceToNow, isToday } from 'date-fns'
-import '../SideBar/SideBar.css'
 
 interface formData {
   avatar: File
@@ -32,9 +32,8 @@ export default function Header() {
   const [user, setUser] = useState<formData | null>(null)
   const [listNotification, setListNotification] = useState<Notification[]>([])
   const [type, setType] = useState('')
-  const [total, setTotal] = useState(0)
   const [page, setPage] = useState(1)
-  const pageSize = 6
+  const pageSize = 20
   const [isOpen, setIsOpen] = useState(false)
   const tabs = ['all', 'notice', 'fee', 'new']
   const tabIndex = tabs.indexOf(type || 'all')
@@ -146,8 +145,6 @@ export default function Header() {
       } else {
         setListNotification((prev) => [...prev, ...newNotifies])
       }
-
-      setTotal(res.data.total)
     } catch (error) {
       console.error('Lỗi khi tải thông báo:', error)
     }
@@ -159,30 +156,7 @@ export default function Header() {
 
   useEffect(() => {
     fetchPageNotification(page)
-  }, [page, type])
-
-  useEffect(() => {
-    const container = notificationRef.current
-    if (!isOpen || !container) return
-
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = container
-
-      if (scrollTop + clientHeight >= scrollHeight - 10) {
-        console.log('đúng hay sai: ', listNotification.length < pageSize)
-
-        if (listNotification.length == pageSize || listNotification.length < pageSize) {
-          setPage((prev) => prev + 1)
-        }
-      }
-    }
-
-    container.addEventListener('scroll', handleScroll)
-
-    return () => {
-      container.removeEventListener('scroll', handleScroll)
-    }
-  }, [isOpen, listNotification, total])
+  }, [page, type, isOpen])
 
   const handleLogout = () => {
     localStorage.removeItem('token')
@@ -251,23 +225,12 @@ export default function Header() {
             className='border border-gray-400 rounded-full pl-10 pr-4 py-2 w-[300px] bg-[#edf2f9] shadow-sm'
           />
         </div>
-        <div className='flex gap-[20px] items-center transition-all duration-300'>
-          {localStorage.getItem('role') === 'manager' && (
-            <button ref={bellRef} onClick={handleNotification} className='relative cursor-pointer'>
-              <Badge badgeContent={countNotification} color='primary'>
-                <div className='p-2 text-gray-600 bg-[#ebf2f5] rounded-full shadow-lg hover:bg-[#eaf3fd] hover:text-[#3385f0] transition-all duration-300 ripple'>
-                  <svg xmlns='http://www.w3.org/2000/svg' width='19' height='19' viewBox='0 0 24 24'>
-                    <path
-                      fill='currentColor'
-                      fillRule='evenodd'
-                      d='M12 1.25A7.75 7.75 0 0 0 4.25 9v.704a3.53 3.53 0 0 1-.593 1.958L2.51 13.385c-1.334 2-.316 4.718 2.003 5.35q1.133.309 2.284.523l.002.005C7.567 21.315 9.622 22.75 12 22.75s4.433-1.435 5.202-3.487l.002-.005a29 29 0 0 0 2.284-.523c2.319-.632 3.337-3.35 2.003-5.35l-1.148-1.723a3.53 3.53 0 0 1-.593-1.958V9A7.75 7.75 0 0 0 12 1.25m3.376 18.287a28.5 28.5 0 0 1-6.753 0c.711 1.021 1.948 1.713 3.377 1.713s2.665-.692 3.376-1.713M5.75 9a6.25 6.25 0 1 1 12.5 0v.704c0 .993.294 1.964.845 2.79l1.148 1.723a2.02 2.02 0 0 1-1.15 3.071a26.96 26.96 0 0 1-14.187 0a2.02 2.02 0 0 1-1.15-3.07l1.15-1.724a5.03 5.03 0 0 0 .844-2.79z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-                </div>
-              </Badge>
-            </button>
-          )}
+        <div className='flex gap-[30px] items-center transition-all duration-300'>
+          <button ref={bellRef} onClick={handleNotification} className='relative cursor-pointer'>
+            <Badge badgeContent={countNotification} color='primary'>
+              <NotificationsIcon style={{ color: '#6C6E71' }} />
+            </Badge>
+          </button>
 
           <div
             ref={notificationRef}
@@ -309,8 +272,8 @@ export default function Header() {
                 <>
                   <p className='text-[14px] font-semibold text-[#1B2124] p-[5px] pl-[10px]'>Today</p>
                   {todayNotifications.map((notify) => (
-                    <div key={notify.id} className='relative hover:bg-[#f1f3f5]'>
-                      <div className='p-[10px] '>
+                    <div className='relative hover:bg-[#f1f3f5]'>
+                      <div key={notify.id} className='p-[10px] '>
                         <div className='flex justify-between ml-[20px]'>
                           <p
                             className='font-bold text-[#1B2124] overflow-hidden text-ellipsis'
@@ -324,7 +287,7 @@ export default function Header() {
                           </p>
 
                           <div
-                            className={`${getTypeColor(notify.type)} w-[80px] h-fit px-2 py-1 rounded-full text-sm font-semibold text-center`}
+                            className={`${getTypeColor(notify.type)} w-[80px] h-fit px-2 py-1 rounded-full text-sm font-semibold text-center w-[30%]`}
                           >
                             <span>{notify.type}</span>
                           </div>
@@ -356,8 +319,8 @@ export default function Header() {
                 <>
                   <p className='text-[14px] font-semibold text-[#1B2124] p-[5px] pl-[10px]'>Older</p>
                   {oldNotifications.map((notify) => (
-                    <div key={notify.id} className='relative hover:bg-[#f1f3f5]'>
-                      <div className='p-[10px] '>
+                    <div className='relative hover:bg-[#f1f3f5]'>
+                      <div key={notify.id} className='p-[10px] '>
                         <div className='flex justify-between ml-[20px]'>
                           <p
                             className='font-bold text-[#1B2124] overflow-hidden text-ellipsis'
@@ -371,7 +334,7 @@ export default function Header() {
                           </p>
 
                           <div
-                            className={`${getTypeColor(notify.type)} w-[80px] h-fit px-2 py-1 rounded-full text-sm font-semibold text-center`}
+                            className={`${getTypeColor(notify.type)} w-[80px] h-fit px-2 py-1 rounded-full text-sm font-semibold text-center w-[30%]`}
                           >
                             <span>{notify.type}</span>
                           </div>
@@ -402,11 +365,10 @@ export default function Header() {
               {listNotification.length === 0 && <p className='p-[10px]'>No notification Foundation</p>}
             </div>
             <div className='flex flex-col align-center items-center pt-[5px] pb-[5px]'>
-              <Link
-                to='/manager/history-notification'
-                className='block text-[#3385F0] font-medium text-center pt-[5px] pb-[5px] w-[200px] hover:bg-[#3385f01f] rounded-[10px]'
-              >
-                View all
+              <Link to='/manager/history-notification'>
+                <a className='block text-[#3385F0] font-medium text-center pt-[5px] pb-[5px] w-[200px] hover:bg-[#3385f01f] rounded-[10px]'>
+                  View all
+                </a>
               </Link>
             </div>
             {/* {listNotification.length < total && (
@@ -417,10 +379,7 @@ export default function Header() {
           <PopupState variant='popover' popupId='demo-popup-popover'>
             {(popupState) => (
               <div>
-                <div
-                  className='w-12 h-12 rounded-full overflow-hidden cursor-pointer shadow-lg'
-                  {...bindTrigger(popupState)}
-                >
+                <div className='w-12 h-12 rounded-full overflow-hidden cursor-pointer' {...bindTrigger(popupState)}>
                   {user?.avatar ? (
                     <img src={getImageSrc(user.avatar)} className='w-full h-full object-cover' />
                   ) : (
--- src/pages/Manager/Incident/UpdateIncident/UpdateIncident.tsx
+++ src/pages/Manager/Incident/UpdateIncident/UpdateIncident.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
+import Button from '@mui/material/Button'
+import Lightbox from 'yet-another-react-lightbox'
+import 'yet-another-react-lightbox/styles.css'
 import { getAllIncident } from '~/apis/incident.api'
 
 export default function UpdateIncident() {
@@ -22,9 +25,175 @@ export default function UpdateIncident() {
     console.log('list: ', listIncident)
   }, [])
 
+  const [lightboxOpen, setLightboxOpen] = useState(false)
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+  const images = [
+    { src: '/public/imgs/avt/ttt.webp' },
+    { src: '/public/imgs/avt/ttt.webp' },
+    { src: '/public/imgs/avt/ttt.webp' },
+    { src: '/public/imgs/avt/ttt.webp' }
+  ]
+
+  const getStatusColor = (status: string) => {
+    switch (status.toLowerCase()) {
+      case 'pending':
+        return 'bg-red-200 text-red-800'
+      case 'in progress':
+        return 'bg-orange-200 text-orange-800'
+      case 'resolved':
+        return 'bg-green-200 text-green-800'
+      default:
+        return ''
+    }
+  }
+
   return (
     <div className='mx-5 mt-5 mb-5 p-6 bg-gradient-to-br from-white via-white to-blue-100 drop-shadow-md rounded-xl'>
-      UpdateIncident
+      <div className='flex justify-between shadow-md border-1 border-[#ccc] bg-white h-[520px]'>
+        <div className='w-[70%] border-r-1 border-[#333] py-[10px] px-[20px] overflow-y-auto custom-scrollbar'>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center gap-[10px]'>
+              <svg xmlns='http://www.w3.org/2000/svg' width='35' height='35' viewBox='0 0 24 24'>
+                <path
+                  fill='currentColor'
+                  d='M7.001 16.154q.328 0 .548-.222t.22-.549t-.221-.548t-.55-.22t-.547.222t-.22.549t.221.548t.55.22M7 13.23q.213 0 .356-.144t.143-.356V8.346q0-.212-.144-.356t-.357-.144t-.356.144t-.143.356v4.385q0 .212.144.356t.357.144m4 1.269h6q.213 0 .356-.144t.144-.357t-.144-.356T17 13.5h-6q-.213 0-.356.144t-.144.357t.144.356t.356.143m0-4h6q.213 0 .356-.144t.144-.357t-.144-.356T17 9.5h-6q-.213 0-.356.144t-.144.357t.144.356t.356.143M4.616 19q-.691 0-1.153-.462T3 17.384V6.616q0-.691.463-1.153T4.615 5h14.77q.69 0 1.152.463T21 6.616v10.769q0 .69-.463 1.153T19.385 19zm0-1h14.769q.23 0 .423-.192t.192-.424V6.616q0-.231-.192-.424T19.385 6H4.615q-.23 0-.423.192T4 6.616v10.769q0 .23.192.423t.423.192M4 18V6z'
+                />
+              </svg>
+              <span className='text-[26px] text-[#4D5969] font-bold'>Detail report</span>
+            </div>
+
+            <span className={`${getStatusColor('pending')} px-2 py-1 rounded-full text-sm font-semibold`}>
+              {/* {incident.status} */}
+              Pending
+            </span>
+          </div>
+
+          <div className='ml-[45px] text-[#29313b] '>
+            <p className='mt-[20px]'>
+              <strong>Type: </strong> An ninh
+            </p>
+            <p className='mt-[20px]'>
+              <strong>Title: </strong> Xuất hiện người lạ
+            </p>
+            <p style={{ textAlign: 'justify' }} className='mt-[20px] leading-[170%] '>
+              Vào khoảng 23h30 ngày 24/04/2025, cư dân tầng 5 tòa nhà A phát hiện một người đàn ông lạ mặt đứng trước
+              cửa căn hộ 504 trong khoảng 10 phút mà không rõ lý do. Người này không mang đồng phục của bất kỳ đơn vị
+              giao hàng hay kỹ thuật nào, liên tục nhìn quanh quan sát hành lang, có biểu hiện lén lút và căng thẳng.
+              Khi bị một cư dân khác hỏi thăm, người này trả lời lấp lửng và sau đó rời khỏi khu vực nhanh chóng. Hình
+              ảnh từ camera hành lang đã ghi nhận đầy đủ hành vi trên.
+            </p>
+          </div>
+
+          <div className='flex items-center gap-[10px] mt-[20px] mb-[20px]'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='35' height='35' viewBox='0 0 48 48'>
+              <path
+                fill='none'
+                stroke='currentColor'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='4'
+                d='M24.707 9.565L9.858 24.415a9 9 0 0 0 0 12.727v0a9 9 0 0 0 12.728 0l17.678-17.677a6 6 0 0 0 0-8.486v0a6 6 0 0 0-8.486 0L14.101 28.657a3 3 0 0 0 0 4.243v0a3 3 0 0 0 4.242 0l14.85-14.85'
+              />
+            </svg>
+            <span className='text-[26px] text-[#4D5969] font-bold'>Image</span>
+          </div>
+          <div className='flex gap-[15px] ml-[45px] flex-wrap'>
+            {images.map((img, index) => (
+              <img
+                key={index}
+                src={img.src}
+                alt={`incident-${index}`}
+                className='w-[125px] h-[125px] rounded-[5px] cursor-pointer'
+                onClick={() => {
+                  setCurrentImageIndex(index)
+                  setLightboxOpen(true)
+                }}
+              />
+            ))}
+
+            <Lightbox
+              open={lightboxOpen}
+              close={() => setLightboxOpen(false)}
+              index={currentImageIndex}
+              slides={images}
+            />
+          </div>
+        </div>
+
+        <div className='w-[27%] py-[20px] text-[14px]'>
+          <div className='mb-[10px]'>
+            <p className='text-[#29313b] mb-[10px]'> Name</p>
+            <input
+              type='text'
+              value='Phan Minh Tuấn'
+              disabled
+              className='w-[85%] px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-600 cursor-not-allowed'
+            />
+          </div>
+
+          <div className='mb-[20px]'>
+            <p className='text-[#29313b] mb-[10px]'> Email</p>
+            <input
+              type='text'
+              value='minhtuan@gmail.com'
+              disabled
+              className='w-[85%] px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-600 cursor-not-allowed'
+            />
+          </div>
+
+          <div className='mb-[20px]'>
+            <p className='text-[#29313b] mb-[10px]'> Phone Number</p>
+            <input
+              type='text'
+              value='0847828516'
+              disabled
+              className='w-[85%] px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-600 cursor-not-allowed'
+            />
+          </div>
+
+          <div className='mb-[20px]'>
+            <p className='text-[#29313b] mb-[10px]'> Apartment</p>
+            <input
+              type='text'
+              value='101A'
+              disabled
+              className='w-[85%] px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-600 cursor-not-allowed'
+            />
+          </div>
+
+          <div className='mb-[20px]'>
+            <label className='block text-sm font-semibold my-2'>Status</label>
+            <select
+              defaultValue='father'
+              className=' w-[85%] h-10 pl-2 cursor-pointer outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+            >
+              <option value='father'>Pending</option>
+              <option value='mother'>Underway</option>
+              <option value='wife'>Complete</option>
+            </select>
+          </div>
+
+          <div className='flex justify-between mt-6 mr-[15%] '>
+            <Button
+              className='w-[120px]'
+              variant='contained'
+              onClick={() => (window.location.href = '/manager/list-incident')}
+              style={{ color: 'white', background: 'red', fontWeight: 'semi-bold' }} // Add this line
+            >
+              Cancel
+            </Button>
+            <Button
+              className='w-[120px]'
+              variant='contained'
+              type='submit'
+              style={{ color: 'white', fontWeight: 'semi-bold' }} // Add this line
+            >
+              Update
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
